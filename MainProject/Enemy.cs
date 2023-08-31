e3e5119c3246980bd09d9372a37d8e60cb0fcc19
DominicBellisario <dlb5102@rit.edu>
2023-08-31 18:21:04 -0400

    started enemy class

--- MainProject/Enemy.cs
+++ MainProject/Enemy.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace MainProject
 {
+    enum enemyState
+    {
+        Walking,
+        Shooting
+    }
     internal class Enemy
     {
+        //how fast the enemy moves
+        private int speed;
+
+        //the aggro radius for the enemy
+        private int aggroRadius;
+
+        //enemy hitbox
+        private Rectangle hitbox;
+
+        //coordinates for the edges of the enemies movement
+        private int leftX;
+        private int leftY;
+        private int rightX;
+        private int rightY;
     }
 }
